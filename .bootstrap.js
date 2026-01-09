e2bc28d514191bc93dddc95d90d4cdca2e69a864
Vibesdk <vibesdk-bot@cloudflare.com>
2026-01-09 12:01:44 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,10 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add react-parallax@^3.2.0",
+    "bun add react-intersection-observer@^9.0.0"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
